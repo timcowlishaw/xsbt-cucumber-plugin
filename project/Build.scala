42c037fe1761a84194e1546ec23423e0f8e85c47
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2014-08-29 15:56:25 +0100

    Build for scala 2.11

--- project/Build.scala
+++ project/Build.scala
@@ -3,8 +3,8 @@ import Keys._
 
 object Settings {
   val buildOrganization = "templemore"
-  val buildScalaVersion = "2.10.2"
-  val crossBuildScalaVersions = Seq("2.9.3", "2.10.2")
+  val buildScalaVersion = "2.11.2"
+  val crossBuildScalaVersions = Seq("2.9.3", "2.10.4", "2.11.2")
   val buildVersion      = "0.8.0"
 
   val buildSettings = Defaults.defaultSettings ++
@@ -13,17 +13,26 @@ object Settings {
                            version       := buildVersion,
                            scalacOptions ++= Seq("-deprecation", "-unchecked", "-encoding", "utf8"),
                            publishTo     := Some(Resolver.file("file",  new File("deploy-repo"))))
+
+  def pluginScalaVersion(scalaVersion : String) =
+    if (scalaVersion.startsWith("2.11")) "2.10.4"
+    else scalaVersion
 }
 
 object Dependencies {
 
   private val CucumberVersion = "1.1.4"
 
-  def cucumberJvm(scalaVersion: String) = 
+  def cucumberJvm(scalaVersion: String) =
     if ( scalaVersion.startsWith("2.9") ) "info.cukes" % "cucumber-scala_2.9" % CucumberVersion % "compile"
+    else if ( scalaVersion.startsWith("2.11")) "info.cukes" % "cucumber-scala_2.10" % CucumberVersion % "compile"
     else "info.cukes" %% "cucumber-scala" % CucumberVersion % "compile"
 
   val testInterface = "org.scala-tools.testing" % "test-interface" % "0.5" % "compile"
+
+  def scalaXml(scalaVersion: String) =
+    if (scalaVersion.startsWith("2.11")) Seq("org.scala-lang.modules" %% "scala-xml" % "1.0.2")
+    else Seq()
 }
 
 object Build extends Build {
@@ -35,10 +44,15 @@ object Build extends Build {
 
   lazy val pluginProject = Project("sbt-cucumber-plugin", file ("plugin"),
     settings = buildSettings ++
-               Seq(crossScalaVersions := Seq.empty, sbtPlugin := true))
+               Seq(
+                 scalaVersion := pluginScalaVersion(buildScalaVersion),
+                 crossScalaVersions := Seq.empty,
+                 sbtPlugin := true,
+                 libraryDependencies <++= scalaVersion { sv => scalaXml(sv) }
+                 ))
 
   lazy val integrationProject = Project ("sbt-cucumber-integration", file ("integration"),
-    settings = buildSettings ++ 
+    settings = buildSettings ++
                Seq(crossScalaVersions := crossBuildScalaVersions,
                libraryDependencies <+= scalaVersion { sv => cucumberJvm(sv) },
                libraryDependencies += testInterface))
